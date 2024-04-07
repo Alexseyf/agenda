ae98b45e968f984fc91049731f25450e4ed474fd
Alexseyf <alexseyf66@gmail.com>
2024-04-07 00:35:09 -0300

    ícones substituídos

--- src/App.jsx
+++ src/App.jsx
@@ -28,21 +28,21 @@ function App() {
         <div className="btn">
           <div className="btn12">
             <div className="btn12__btn1" onClick={mensagem}>
-              <span class="material-symbols-outlined">chat</span>
+              <img src="basic_message_multiple.svg" alt="" />
             </div>
             <div className="btn12__btn2" onClick={calendario}>
-              <span class="material-symbols-outlined">calendar_month</span>
+              <img src="basic_calendar.svg" alt="" />
             </div>
           </div>
           <div className="btn__btn5" onClick={agenda}>
-            <span class="material-symbols-outlined">library_books</span>
+            <img src="basic_notebook.svg" alt="" />
           </div>
           <div className="btn34">
             <div className="btn34__btn3" onClick={atividades}>
-              <span class="material-symbols-outlined">school</span>
+              <img src="basic_pencil_ruler_pen .svg" alt="" />
             </div>
             <div className="btn34__btn4" onClick={info}>
-              <span class="material-symbols-outlined">help_center</span>
+              <img src="basic_info.svg" alt="" />
             </div>
           </div>
         </div>
